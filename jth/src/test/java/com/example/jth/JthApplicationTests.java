519a08bf3f7d9185fae44098c5df510ae37e5608
Minjae-An <mj3242@naver.com>
2023-09-13 19:54:05 +0900

    feat : 프로젝트 초기 환경 설정

---
+++ jth/src/test/java/com/example/jth/JthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
