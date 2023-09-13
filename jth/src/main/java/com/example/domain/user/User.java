7eeb03186b2195cf0cf8e45b07106e69686b300c
Minjae-An <mj3242@naver.com>
2023-09-13 20:03:05 +0900

    feat : User 클래스 추가(#2)
    유저를 표현하기 위한 User 엔티티 클래스 추가

---
+++ jth/src/main/java/com/example/domain/user/User.java
@@ -0,0 +1,29 @@
+package com.example.domain.user;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String password;
+    private Gender gender;
+    private String phoneNumber;
+
+    public User(String name, String password, Gender gender, String phoneNumber) {
+        this.name = name;
+        this.password = password;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+}
