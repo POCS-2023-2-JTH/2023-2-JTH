6d3ff2d1b76f5d7b93ecb507ace4a33b02b6a0dd
Minjae-An <mj3242@naver.com>
2023-09-15 20:46:35 +0900

    feat : UserController getUserDetail 추가(#2)
    회원 상세 정보 제공을 위한 메서드 정의.
    ResponseEntity와 UserDetailResponse를 활용하여 응답.

--- jth/src/main/java/com/example/jth/controller/UserController.java
+++ jth/src/main/java/com/example/jth/controller/UserController.java
@@ -2,12 +2,11 @@ package com.example.jth.controller;
 
 import com.example.jth.dto.join.JoinRequest;
 import com.example.jth.dto.join.JoinResponse;
+import com.example.jth.dto.user_detail.UserDetailResponse;
 import com.example.jth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -18,9 +17,15 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<JoinResponse> join(@RequestBody @Valid JoinRequest joinRequest){
+    public ResponseEntity<JoinResponse> join(@RequestBody @Valid JoinRequest joinRequest) {
         JoinResponse response = userService.join(joinRequest);
         URI uri = URI.create("/user/detail/" + response.getId());
         return ResponseEntity.created(uri).body(response);
+    }
+
+    @GetMapping("/user/detail/{id}")
+    public ResponseEntity<UserDetailResponse> getUserDetail(@PathVariable Long id) {
+        UserDetailResponse userDetail = userService.getUserDetail(id);
+        return ResponseEntity.ok(userDetail);
     }
 }
