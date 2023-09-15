bf0144d9b26e6f3a94f54f555fb8f902a30e3f2b
Minjae-An <mj3242@naver.com>
2023-09-15 22:24:34 +0900

    feat : UserController updateUserDetail 추가(#4)
    유저 정보 수정 메서드 추가.

--- jth/src/main/java/com/example/jth/controller/UserController.java
+++ jth/src/main/java/com/example/jth/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.jth.dto.join.JoinRequest;
 import com.example.jth.dto.join.JoinResponse;
 import com.example.jth.dto.user_detail.UserDetailResponse;
 import com.example.jth.dto.user_leave.UserLeaveRequest;
+import com.example.jth.dto.user_update.UserUpdateRequest;
+import com.example.jth.dto.user_update.UserUpdateResponse;
 import com.example.jth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -27,13 +29,19 @@ public class UserController {
 
     @GetMapping("/user/detail/{id}")
     public ResponseEntity<UserDetailResponse> getUserDetail(@PathVariable @NotNull Long id) {
-        UserDetailResponse userDetail = userService.getUserDetail(id);
-        return ResponseEntity.ok(userDetail);
+        UserDetailResponse response = userService.getUserDetail(id);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/user/detail/delete")
     public ResponseEntity<String> userLeave(@RequestBody @Valid UserLeaveRequest request){
         userService.userLeave(request);
         return ResponseEntity.accepted().body("member successfully leaved");
+    }
+
+    @PostMapping("/user/detail/update")
+    public ResponseEntity<UserUpdateResponse> updateUserDetail(@RequestBody @Valid UserUpdateRequest request){
+        UserUpdateResponse response = userService.updateUserDetail(request);
+        return ResponseEntity.accepted().body(response);
     }
 }
