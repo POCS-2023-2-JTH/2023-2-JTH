4b86d8079aea6b6f67d0840046768d88cd9526b0
Minjae-An <mj3242@naver.com>
2023-09-19 19:47:29 +0900

    feat : PostController getPost 로직 추가(#3)

--- jth/src/main/java/com/example/jth/controller/PostController.java
+++ jth/src/main/java/com/example/jth/controller/PostController.java
@@ -1,14 +1,17 @@
 package com.example.jth.controller;
 
+import com.example.jth.dto.post_page.PostDTO;
 import com.example.jth.dto.post_page.PostPageRequest;
 import com.example.jth.dto.post_page.PostPageResponse;
 import com.example.jth.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +21,12 @@ public class PostController {
     @GetMapping("/posts")
     public ResponseEntity<PostPageResponse> getPosts(@Valid PostPageRequest request){
         PostPageResponse response = postService.getPosts(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<PostDTO> getPost(@PathVariable @Positive Long postId){
+        PostDTO response = postService.getPost(postId);
         return ResponseEntity.ok(response);
     }
 
