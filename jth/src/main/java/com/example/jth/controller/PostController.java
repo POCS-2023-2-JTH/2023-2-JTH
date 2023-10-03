72c81befd43dd2ffaa82931bf746b54246e62b24
Minjae-An <mj3242@naver.com>
2023-10-03 21:35:53 +0900

    feat : PostController searchPosts 메서드 추가(#3)
    게시물 검색 기능 제공을 위한 로직 추가

--- jth/src/main/java/com/example/jth/controller/PostController.java
+++ jth/src/main/java/com/example/jth/controller/PostController.java
@@ -6,6 +6,7 @@ import com.example.jth.dto.post_page.PostDTO;
 import com.example.jth.dto.post_page.PostPageRequest;
 import com.example.jth.dto.post_page.PostPageResponse;
 import com.example.jth.dto.remove_post.DeletePostRequest;
+import com.example.jth.dto.search_post.SearchPostRequest;
 import com.example.jth.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,27 +22,32 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts")
-    public ResponseEntity<PostPageResponse> getPosts(@Valid PostPageRequest request){
+    public ResponseEntity<PostPageResponse> getPosts(@Valid PostPageRequest request) {
         PostPageResponse response = postService.getPosts(request);
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/post/{postId}")
-    public ResponseEntity<PostDTO> getPost(@PathVariable @Positive Long postId){
+    public ResponseEntity<PostDTO> getPost(@PathVariable @Positive Long postId) {
         PostDTO response = postService.getPost(postId);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/post/write")
-    public ResponseEntity<AddPostResponse> addPost(@RequestBody @Valid AddPostRequest request){
+    public ResponseEntity<AddPostResponse> addPost(@RequestBody @Valid AddPostRequest request) {
         AddPostResponse response = postService.addPost(request);
         return ResponseEntity.accepted().body(response);
     }
 
     @PostMapping("/post/delete")
-    public ResponseEntity<Objects> deletePost(@RequestBody @Valid DeletePostRequest request){
+    public ResponseEntity<Objects> deletePost(@RequestBody @Valid DeletePostRequest request) {
         postService.deletePost(request);
         return ResponseEntity.accepted().build();
     }
 
+    @GetMapping("/post")
+    public ResponseEntity<PostPageResponse> searchPosts(@Valid SearchPostRequest request) {
+        PostPageResponse response = postService.search(request);
+        return ResponseEntity.ok(response);
+    }
 }
