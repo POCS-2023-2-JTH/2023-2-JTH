cbce1701ddcd8223c8fc11bf86844c058aa8ee27
Minjae-An <mj3242@naver.com>
2023-10-26 21:28:52 +0900

    feat : 인터셉터 관련 설정 추가(#1)
    인증 처리를 위한 LoginInterceptor 추가에 따라 해당
    인터셉터가 정상적으로 동작하도록 addInterceptors를 오버라이딩

--- jth/src/main/java/com/example/jth/config/WebConfig.java
+++ jth/src/main/java/com/example/jth/config/WebConfig.java
@@ -1,12 +1,18 @@
 package com.example.jth.config;
 
+import com.example.jth.interceptor.LoginInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -15,5 +21,12 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name())
                 .allowedHeaders("*");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/join", "/login");
     }
 }
