0d631f367b08f8b528603ff42bd203d9efd03416
Minjae-An <mj3242@naver.com>
2023-10-26 21:23:52 +0900

    feat : LoginController 정의(#1)
    로그인 기능 제공을 위해 컨트롤러 클래스 정의.
    서비스 계층에서 올바르게 로그인 로직이 처리될 경우,
    세션에 인증 정보를 유저의 PK롤 설정하여 리소스 접근이
    가능케 한다.

---
+++ jth/src/main/java/com/example/jth/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.jth.controller;
+
+import com.example.jth.dto.login.LoginRequest;
+import com.example.jth.dto.login.LoginResponse;
+import com.example.jth.service.LoginService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginController {
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest loginRequest,
+                                               HttpServletRequest request){
+        LoginResponse response = loginService.login(loginRequest);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("LOGIN_MEMBER", response.getUserId());
+
+        return ResponseEntity.ok(response);
+    }
+}
