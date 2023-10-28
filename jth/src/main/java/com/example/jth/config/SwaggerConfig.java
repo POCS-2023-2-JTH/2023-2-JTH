63e7fd78d3185183801fd433465512b08a2ea98d
Minjae-An <mj3242@naver.com>
2023-10-28 22:12:25 +0900

    feat : SwaggerConfig addResourceHandler 수정(#21)
    잘못된 swagger-ui.html 자원 경로 수정

--- jth/src/main/java/com/example/jth/config/SwaggerConfig.java
+++ jth/src/main/java/com/example/jth/config/SwaggerConfig.java
@@ -46,7 +46,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources");
+                .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
