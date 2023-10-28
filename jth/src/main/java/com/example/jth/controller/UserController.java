8050cc5327b70e660df33940c0553631d6143270
Minjae-An <mj3242@naver.com>
2023-10-28 22:12:57 +0900

    feat : UserController API 명세(#21)
    @ApiOperation 이용하여 User 관련 API 명세

--- jth/src/main/java/com/example/jth/controller/UserController.java
+++ jth/src/main/java/com/example/jth/controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.jth.dto.user_leave.UserLeaveRequest;
 import com.example.jth.dto.user_update.UserUpdateRequest;
 import com.example.jth.dto.user_update.UserUpdateResponse;
 import com.example.jth.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
@@ -22,24 +23,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(value = "회원가입 api", notes = "새로운 회원 데이터 저장")
     @PostMapping("/join")
     public ResponseEntity<JoinResponse> join(@RequestBody @Valid JoinRequest joinRequest) {
         JoinResponse response = userService.join(joinRequest);
         return ResponseEntity.ok(response);
     }
 
+    @ApiOperation(value = "회원 상세 정보 api", notes = "회원 상세 정보 조회")
     @GetMapping("/user/detail/{id}")
     public ResponseEntity<UserDetailResponse> getUserDetail(@PathVariable @NotNull Long id) {
         UserDetailResponse response = userService.getUserDetail(id);
         return ResponseEntity.ok(response);
     }
 
+    @ApiOperation(value = "회원 탈퇴 api", notes = "회원 탈퇴(회원 데이터 삭제)")
     @PostMapping("/user/detail/delete")
     public ResponseEntity<String> userLeave(@RequestBody @Valid UserLeaveRequest request){
         userService.userLeave(request);
         return ResponseEntity.accepted().body("member successfully leaved");
     }
 
+    @ApiOperation(value = "회원 정보 수정 api", notes = "회원 정보 수정")
     @PostMapping("/user/detail/update")
     public ResponseEntity<UserUpdateResponse> updateUserDetail(@RequestBody @Valid UserUpdateRequest request){
         UserUpdateResponse response = userService.updateUserDetail(request);
