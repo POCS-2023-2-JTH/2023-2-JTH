286c978f8f1527ccecbc04d879dac8f18e7a1861
drimh <107299318+drimh@users.noreply.github.com>
2023-10-31 01:39:34 +0900

    feat: 게시판 백엔드 통신 완료

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Join from "./join"; 
 import Login from "./Login"; 
 import BoardCreate from "./BoardCreate"; 
-import BoardList from "./BoardList"; 
 import BoardDetail from "./BoardDetail"; 
+import Board from "./Board";
 
 function App() {
   return (
@@ -11,10 +11,9 @@ function App() {
       <Routes>
         <Route path="/join" element={<Join />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/posts" element={<Board />} />
         <Route path="/post/write" element={<BoardCreate />} />
         <Route path="/post/:postId" element={<BoardDetail />} />
-        {/* <Route path="/boardList" element={<BoardList />} /> */}
-        {/* <Route path="/board" element={<Board />} /> */}
       </Routes>
   </Router>
   );
--- my-app/src/Board.js
+++ my-app/src/Board.js
@@ -1,24 +1,37 @@
-import React, { useState } from 'react';
-// import BoardCreate from './BoardCreate';
-// import BoardList from './BoardList';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import "./Board.css";
 
-const Board = ({ title, category, content }) => {
-//   const [posts, setPosts] = useState([]);
+export default function Board() {
+    const [posts, setPosts] = useState(null);
 
-//   const handlePostSubmit = (newPost) => {
-//     setPosts([...posts, newPost]);
-//   }
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get('http://15.164.107.242:8080/posts?page=0&size=10');
+                setPosts(response.data.posts);
+            } catch (error) {
+                console.error('Failed to fetch posts:', error);
+            }
+        };
 
-  return (
-    <div>
+        fetchPosts();
+    }, []);
+
+    if (!posts) return null;  // posts가 아직 정의되지 않았다면 아무것도 렌더링하지 않음
+
+    return (
         <div>
-            <h2>{title}</h2>
-            <h5>{category}</h5>
-            <hr />
-            <p>{content}</p>
+            {posts.map((post) => (
+                <div key={post.id}>
+                    <p>번호:{post.id}</p>
+                    <p>카테고리:{post.category}</p>
+                    <p>제목:{post.title}</p>
+                    <p>작성자: {post.userName}</p>
+                    <p>등록일: {post.created}</p>
+                </div>
+            ))}
         </div>
-    </div>
-  );
+    );
 }
-
-export default Board;
